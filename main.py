9a8bb772e345e359c750cc227290c6a1a3515c2a
apix3 <98614093+apix3@users.noreply.github.com>
2022-10-24 18:20:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def on_ready():
 
 
 
-@bot.slash_command(guild_ids=[908188649206718504])
+@bot.slash_command(guild_ids=[testingServerId])
 @commands.has_role('Panel Member')
 async def convict(interaction : nextcord.Interaction, username:str, reason:str, channelid:str):
     user = await roblox.get_user_by_username(username)
@@ -78,5 +78,41 @@ async def convict(interaction : nextcord.Interaction, username:str, reason:str, 
     await interaction.response.send_message(embed=embed)
 
 
+@bot.slash_command(guild_ids=[testingServerId])
+@commands.has_role('Panel Member')
+async def watchlist(interaction : nextcord.Interaction, username:str, reason:str, channelid:str):
+    user = await roblox.get_user_by_username(username)
+    embed = nextcord.Embed(title=f"{user.name}", color=0xda0b0b, url=user.profile_url)
+
+    embed.add_field(
+        name = "Username",
+        value= f"{user.name}",
+        inline = False
+    )
+
+    embed.add_field(
+        name = "Reason",
+        value= f"{reason}",
+        inline=False
+    )
+
+    embed.add_field(
+        name = "Proof",
+        value=f"<#" + str(channelid) + ">",
+        inline= False
+    )
+
+    avatar_image = await user.thumbnails.get_avatar_image(
+    shot_type=ThumbnailType.avatar_headshot,  # headshot
+    size=ThumbnailSize.size_420x420,  # high resolution thumbnail
+    is_circular=False  # square thumbnail
+    )
+    embed.set_thumbnail(
+    url=avatar_image
+    )
+
+    embed.set_footer(text=f"Adure Cheater Registery")
+
+    await interaction.response.send_message(embed=embed)
 
 bot.run(TOKEN)
